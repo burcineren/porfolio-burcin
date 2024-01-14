68f533e3a8baac4733ed64b24828a72657543b40
burcineren <burcin.ern969@gmail.com>
2024-01-14 22:41:17 +0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { LayoutModule } from './modules/layout/layout.module';
 import { HomeComponent } from './public/components/home/home.component';
+import { AboutComponent } from './public/components/about/about.component';
+import { TechStackComponent } from './public/components/tech-stack/tech-stack.component';
+import { ProjectsComponent } from './public/components/projects/projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    TechStackComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -2,16 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AboutComponent } from './components/about/about.component';
 import { HomeComponent } from './components/home/home.component';
+import { TechStackComponent } from './components/tech-stack/tech-stack.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
 
 
 @NgModule({
   declarations: [
     AboutComponent,
-    HomeComponent
+    HomeComponent,
+    TechStackComponent,
+    ProjectsComponent
   ],
   imports: [
     CommonModule,
-  ]
+  ],
+  exports: [AboutComponent]
 })
 export class PublicModule { }
